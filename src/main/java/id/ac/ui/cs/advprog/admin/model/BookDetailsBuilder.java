ded0105025964e935047bb671c8bc183fe51f004
Athar Adista <athar0165@gmail.com>
2024-04-17 20:16:22 +0700

    [RED] Add BookDetailsBuilder model skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/admin/model/BookDetailsBuilder.java
@@ -0,0 +1,66 @@
+package id.ac.ui.cs.advprog.admin.model;
+
+public class BookDetailsBuilder {
+    package id.ac.ui.cs.advprog.admin.model;
+
+    public class BookBuilder {
+        private String bookId;
+        private String title;
+        private String author;
+        private String publisher;
+        private double price;
+        private String description;
+        private int stock;
+        private String publishDate;
+        private String ISBN;
+        private int pages;
+        private String coverPicture;
+        private String category;
+        private int sold;
+
+        public BookBuilder bookId(String bookId){
+        }
+
+        public BookBuilder title(String title){
+        }
+
+        public BookBuilder author(String author){
+        }
+
+        public BookBuilder publisher(String publisher){
+        }
+
+        public BookBuilder price(double price){
+        }
+
+        public BookBuilder description(String description){
+        }
+
+        public BookBuilder stock(int stock){
+        }
+
+        public BookBuilder publishDate(String publishDate){
+        }
+
+        public BookBuilder ISBN(String ISBN){
+        }
+
+        public BookBuilder pages(int pages){
+        }
+
+        public BookBuilder coverPicture(String coverPicture){
+        }
+
+        public BookBuilder category(String category){
+        }
+
+        public BookBuilder sold(int sold){
+        }
+
+        public Book build(){
+
+        };
+
+    }
+
+}
