cb51ca87c8da0d3947c8d3922f1e81ba04ba08d0
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-13 23:42:33 +0700

    [RED] update model customer

--- src/main/java/id/ac/ui/cs/advprog/admin/model/Customer.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/model/Customer.java
@@ -1,17 +1,33 @@
 package id.ac.ui.cs.advprog.admin.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Setter;
+
 
 @Entity
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private String phoneNumber;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    int id;
+
+    @Column(name = "name", updatable = false, nullable = false)
+    @Setter(AccessLevel.NONE)
+    String name;
+
+    @Column(name = "email", updatable = false, nullable = false)
+    @Setter(AccessLevel.NONE)
+    String email;
+
+    @Column(name = "phoneNumber")
+    int phoneNumber;
+
+    public Customer(int id, String name, String email, int phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 
 }
