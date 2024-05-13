670e81ae9ba726c7fda3266560c9e8af577121d5
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-13 23:20:42 +0700

    update customer controller

--- src/main/java/id/ac/ui/cs/advprog/admin/controller/CustomerController.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/controller/CustomerController.java
@@ -1,23 +1,21 @@
 package id.ac.ui.cs.advprog.admin.controller;
+import id.ac.ui.cs.advprog.admin.model.Customer;
 import id.ac.ui.cs.advprog.admin.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/api/customer")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping
-    public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
-        List<CustomerDTO> customers = customerService.getAllCustomers();
-        return new ResponseEntity<>(customers, HttpStatus.OK);
+    @GetMapping("/")
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
     }
 
     @GetMapping("/{id}")
