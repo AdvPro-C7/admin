33b4a4192bf4af6908f3e79d779f71740c5c4023
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-13 23:34:28 +0700

    [RED] update buku controller

--- src/main/java/id/ac/ui/cs/advprog/admin/controller/DaftarBukuController.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/controller/DaftarBukuController.java
@@ -1,57 +1,34 @@
 package id.ac.ui.cs.advprog.admin.controller;
 import id.ac.ui.cs.advprog.admin.model.Buku;
-import id.ac.ui.cs.advprog.admin.repository.BukuRepository;
+import id.ac.ui.cs.advprog.admin.services.BukuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
-public class BukuController {
+@RequestMapping("/api/buku")
+public class BukuController<BukuDTO> {
     @Autowired
-    private BukuRepository bukuRepository;
+    private BukuService bukuService;
 
-    @GetMapping
-    public ResponseEntity<List<Buku>> getAllBuku() {
-        List<Buku> buku = bukuRepository.findAll();
-        return new ResponseEntity<>(buku, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Buku> addBuku(@RequestBody Buku buku) {
-        Buku savedBuku = (Buku) bukuRepository.save(buku);
-        return new ResponseEntity<>(savedBuku, HttpStatus.CREATED);
+    @GetMapping("/")
+    public List<BukuDTO> getAllBuku() {
+        return bukuService.getAllBuku();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Buku> getBukuById(@PathVariable Long id) {
-        Buku buku = bukuRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Buku tidak ditemukan " + id));
-        return new ResponseEntity<>(buku, HttpStatus.OK);
+    public Buku getBukuById(@PathVariable int id) {
+        return bukuService.getBukuById(id);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Buku> updateBuku(@PathVariable Long id, @RequestBody Buku bukuDetails) {
-        Buku buku = bukuRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Buku tidak ditemukan " + id));
-
-        buku.setJudul(bukuDetails.getJudul());
-        buku.setAuthor(bukuDetails.getAuthor());
-        buku.setPublisher(bukuDetails.getPublisher());
-        buku.setHarga(bukuDetails.getHarga());
-
-        Buku updatedBuku = bukuRepository.save(buku);
-        return new ResponseEntity<>(updatedBuku, HttpStatus.OK);
+    @PostMapping("/")
+    public Buku saveOrUpdateBuku(@RequestBody Buku buku) {
+        return bukuService.saveOrUpdateBuku(buku);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteBuku(@PathVariable Long id) {
-        Buku buku = bukuRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Buku tidak ditemukan " + id));
-        bukuRepository.delete(buku);
-        return ResponseEntity.ok().build();
+    public void deleteBuku(@PathVariable int id) {
+        bukuService.deleteBuku((long) id);
     }
 }
