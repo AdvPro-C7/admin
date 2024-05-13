9b21b26c765ac663083a66075f18f43f613e19b6
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-13 23:52:54 +0700

    [RED] update Customer service impl

--- src/main/java/id/ac/ui/cs/advprog/admin/services/CustomerServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/services/CustomerServiceImpl.java
@@ -1,65 +1,35 @@
 package id.ac.ui.cs.advprog.admin.services;
-import id.ac.ui.cs.advprog.admin.dto.CustomerDto;
 import id.ac.ui.cs.advprog.admin.model.Customer;
 import id.ac.ui.cs.advprog.admin.repository.CustomerRepository;
-import id.ac.ui.cs.advprog.admin.services.CustomerService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService {
 
         @Autowired
         private CustomerRepository customerRepository;
 
         @Override
-        public List<CustomerDTO> getAllCustomers() {
-            return customerRepository.findAll().stream()
-                    .map(this::convertToDTO)
-                    .collect(Collectors.toList());
+        public List<Customer> getAllCustomers() {
+            return customerRepository.findAll();
         }
 
         @Override
-        public CustomerDTO getCustomerById(Long id) {
-            Customer customer = customerRepository.findById(id).orElse(null);
-            return convertToDTO(customer);
+        public Customer getCustomerById(int id) {
+            Optional<Customer> customerOptional = customerRepository.findById(id);
+            return customerOptional.orElse(null);
         }
 
         @Override
-        public CustomerDTO createCustomer(CustomerDTO customerDTO) {
-            Customer customer = convertToEntity(customerDTO);
-            Customer savedCustomer = customerRepository.save(customer);
-            return convertToDTO(savedCustomer);
+        public Customer saveOrUpdateCustomer(Customer customer) {
+            return customerRepository.save(customer);
         }
 
         @Override
-        public CustomerDTO updateCustomer(Long id, CustomerDTO customerDTO) {
-            Customer existingCustomer = customerRepository.findById(id).orElse(null);
-            if (existingCustomer != null) {
-                BeanUtils.copyProperties(customerDTO, existingCustomer);
-                Customer updatedCustomer = customerRepository.save(existingCustomer);
-                return convertToDTO(updatedCustomer);
-            }
-            return null;
-        }
-
-        @Override
-        public void deleteCustomer(Long id) {
+        public void deleteCustomer(int id) {
             customerRepository.deleteById(id);
-        }
-
-        private CustomerDTO convertToDTO(Customer customer) {
-            CustomerDTO customerDTO = new CustomerDTO();
-            BeanUtils.copyProperties(customer, customerDTO);
-            return customerDTO;
-        }
-
-        private Customer convertToEntity(CustomerDTO customerDTO) {
-            Customer customer = new Customer();
-            BeanUtils.copyProperties(customerDTO, customer);
-            return customer;
         }
     }
 
