b91d54f1a8bbdc69d3425f4f4834428e47afcb2e
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-18 16:14:20 +0700

    [RED] impl completable customer serviceimpl

--- src/main/java/id/ac/ui/cs/advprog/admin/services/CustomerServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/services/CustomerServiceImpl.java
@@ -1,34 +1,42 @@
 package id.ac.ui.cs.advprog.admin.services;
 import id.ac.ui.cs.advprog.admin.model.Customer;
+import id.ac.ui.cs.advprog.admin.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CustomerServiceImpl implements CustomerService {
 
         @Autowired
-        private id.ac.ui.cs.advprog.admin.dto.CustomerRepository customerRepository;
+        private CustomerRepository customerRepository;
+        private final Executor executor = Executors.newFixedThreadPool(10);
 
         @Override
-        public List<Customer> getAllCustomers() {
-            return customerRepository.findAll();
+        public CompletableFuture<List<Customer>> getAllCustomers() {
+            return CompletableFuture.supplyAsync(() -> customerRepository.findAll(), executor);
         }
 
         @Override
-        public Customer getCustomerById(int id) {
-            Optional<Customer> customerOptional = customerRepository.findById((long) id);
-            return customerOptional.orElse(null);
+        public CompletableFuture<Customer> getCustomerById(int id) {
+            return CompletableFuture.supplyAsync(() -> {
+                Optional<Customer> customerOptional = customerRepository.findById((long) id);
+                return customerOptional.orElse(null);
+            }, executor);
         }
 
         @Override
-        public Customer saveOrUpdateCustomer(Customer customer) {
-            return customerRepository.save(customer);
+        public CompletableFuture<Customer> saveOrUpdateCustomer(Customer customer) {
+            return CompletableFuture.supplyAsync(() -> customerRepository.save(customer), executor);
         }
 
         @Override
-        public void deleteCustomer(int id) {
-            customerRepository.deleteById((long) id);
+        public CompletableFuture<Void> deleteCustomer(int id) {
+            return CompletableFuture.runAsync(() -> customerRepository.deleteById((long) id), executor);
         }
     }
 
