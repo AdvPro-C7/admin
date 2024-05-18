379f30e26a14315cf6f188b2e048e2f69b066926
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-18 16:12:11 +0700

    [RED] customer service impl completable

--- src/main/java/id/ac/ui/cs/advprog/admin/services/CustomerService.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/services/CustomerService.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.admin.services;
 import id.ac.ui.cs.advprog.admin.model.Customer;
-
+import java.util.concurrent.CompletableFuture;
 import java.util.List;
 
 
 public interface CustomerService {
-    List<Customer> getAllCustomers();
-    Customer getCustomerById(int id);
-    Customer saveOrUpdateCustomer(Customer customer);
-    void deleteCustomer(int id);
+    CompletableFuture<List<Customer>> getAllCustomers();
+    CompletableFuture<Customer> getCustomerById(int id);
+    CompletableFuture<Customer> saveOrUpdateCustomer(Customer customer);
+    CompletableFuture<Void> deleteCustomer(int id);
 }
