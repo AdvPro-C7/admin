a043fba05a07caa769c1555b4a65db75d69402aa
josephine-ceria <Josephine.ceria@ui.ac.id>
2024-05-24 23:27:12 +0700

    [REFRACTOR] penyesuaian variabel

--- src/main/java/id/ac/ui/cs/advprog/admin/repository/BukuRepository.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.admin.repository;
-
-import id.ac.ui.cs.advprog.admin.model.Buku;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BukuRepository extends JpaRepository<Buku, Integer> {
-}
